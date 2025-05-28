89bc7bfb02ffc9403f6e63028c71b5b2d3f3a4b4
maxawake <max.m.richter@protonmail.com>
2025-05-29 00:14:47 +0200

    Working vtk filter template

---
+++ src/paraview-plugins/MyProject/MyModule/MyAlgorithm.cpp
@@ -0,0 +1,22 @@
+#include "MyAlgorithm.h"
+#include <vtkObjectFactory.h>
+#include <vtkPolyData.h>
+
+vtkStandardNewMacro(MyAlgorithm);
+
+MyAlgorithm::MyAlgorithm() {
+  this->SetNumberOfInputPorts(1);
+  this->SetNumberOfOutputPorts(1);
+}
+
+int MyAlgorithm::RequestData(vtkInformation* request,
+                             vtkInformationVector** inputVector,
+                             vtkInformationVector* outputVector) {
+  vtkPolyData* input = vtkPolyData::GetData(inputVector[0], 0);
+  vtkPolyData* output = vtkPolyData::GetData(outputVector, 0);
+  if (!input || !output)
+    return 0;
+
+  output->ShallowCopy(input);
+  return 1;
+}
